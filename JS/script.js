eba08161b7280fc14513f71f65fd2345ed5a5a62
SimonePresti <prestisimone96@gmail.com>
2022-11-21 17:07:52 +0100

    Fixed js

--- JS/script.js
+++ JS/script.js
@@ -5,12 +5,21 @@
 //    - va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va emesso (presentato all'utente) in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+const userName = prompt ('Come ti chiami?')
+
+console.log (`Come si chiama il cliente: ${userName}`)
 
 const km = prompt ('Quanti km vuoi percorrere?');
 
+console.log (`Quanti km vuole percorrere il cliente: ${km}`)
+
 const age = prompt ('Quanti anni hai?');
 
+console.log (`Quanti anni ha il cliente: ${age}`)
+
 let howManyKm = (0.21 * km);
+
+console.log (`Il prezzo base del biglietto al km è di: ${howManyKm.toFixed(2)}€ `);
 
 let minors = (age < 18);
 
@@ -20,23 +29,23 @@ let senior = (age > 65);
 
 let discountMayors = ((howManyKm / 100) * 40);
 
-let ticketCost;
+let ticketCost = howManyKm
 
-console.log (km, age, howManyKm, minors, senior, discountMinors, discountMayors );
 
-if (age <= 18) {
+
+
+if (age < 18) {
    ticketCost = (howManyKm - discountMinors);
 
-} else if (age >= 65) {
+} else if (age > 65) {
     ticketCost = (howManyKm - discountMayors);
 
-} else {
-    ticketCost = howManyKm;
-}
+}  
 
 
 
+console.log (`Il prezzo del biglietto scontato è di: ${ticketCost.toFixed(2)}€ `)
 
-document.getElementById ('cost').innerHTML =`Il prezzo del tuo biglietto è di: ${ticketCost.toFixed(2)}&euro;`;
+document.getElementById ('cost').innerHTML =`Ciao ${userName} Il prezzo del tuo biglietto è di: ${ticketCost.toFixed(2)}&euro;`;
 
 
